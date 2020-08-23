b0c701589bca8797d4cee0779a443cfbb07cb36c
smith-30 <kohei.misu@techtouch.co.jp>
2020-08-24 00:54:00 +0900

    mod: set parseTime=true

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/bxcodec/faker"
 	"github.com/smith-30/ent-playground/ent"
+	"github.com/smith-30/ent-playground/ent/car"
 	"github.com/smith-30/ent-playground/ent/user"
 
 	// mysql driver
@@ -18,7 +20,7 @@ type FakeData struct {
 }
 
 func main() {
-	client, err := ent.Open("mysql", "root:ent@tcp(localhost:13307)/ent_sample")
+	client, err := ent.Open("mysql", "root:ent@tcp(localhost:13307)/ent_sample?parseTime=true")
 	if err != nil {
 		log.Fatalf("failed connecting to mysql: %v", err)
 	}
@@ -49,6 +51,18 @@ func main() {
 		return
 	}
 	fmt.Printf("%#v\n", fu)
+
+	fuu, err := CreateCars(ctx, client, fa.Name)
+	if err != nil {
+		fmt.Printf("%#v\n", err)
+		return
+	}
+	fmt.Printf("%#v\n", fuu)
+
+	if err := QueryCars(ctx, fuu); err != nil {
+		fmt.Printf("%#v\n", err)
+		return
+	}
 }
 
 func CreateUser(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
@@ -76,4 +90,74 @@ func QueryUser(ctx context.Context, client *ent.Client, name string) (*ent.User,
 	}
 	log.Println("user returned: ", u)
 	return u, nil
+}
+
+func CreateCars(ctx context.Context, client *ent.Client, userName string) (*ent.User, error) {
+	// creating new car with model "Tesla".
+	tesla, err := client.Car.
+		Create().
+		SetModel("Tesla").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %v", err)
+	}
+
+	// creating new car with model "Ford".
+	ford, err := client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %v", err)
+	}
+	log.Println("car was created: ", ford)
+
+	// create a new user, and add it the 2 cars.
+	a8m, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		AddCars(tesla, ford).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %v", err)
+	}
+	log.Println("user was created: ", a8m)
+	return a8m, nil
+}
+
+func QueryCars(ctx context.Context, a8m *ent.User) error {
+	cars, err := a8m.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("QueryCars().All failed querying user cars: %v", err)
+	}
+	log.Println("returned cars:", cars)
+
+	// what about filtering specific cars.
+	ford, err := a8m.QueryCars().
+		Where(car.ModelEQ("Ford")).
+		Only(ctx)
+	if err != nil {
+		return fmt.Errorf("QueryCars failed querying user cars: %v", err)
+	}
+	log.Println(ford)
+	return nil
+}
+
+func QueryCarUsers(ctx context.Context, a8m *ent.User) error {
+	cars, err := a8m.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %v", err)
+	}
+	// query the inverse edge.
+	for _, ca := range cars {
+		owner, err := ca.QueryOwner().Only(ctx)
+		if err != nil {
+			return fmt.Errorf("failed querying car %q owner: %v", ca.Model, err)
+		}
+		log.Printf("car %q owner: %q\n", ca.Model, owner.Name)
+	}
+	return nil
 }
